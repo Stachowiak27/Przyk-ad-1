408380ca760267bf7b58093ff19ae5182955379e
Sebastian <stachowiak.mail@wp.pl>
2019-11-19 20:33:18 +0100

    konfiguracja projektu

---
+++ PYTHON/Projekt_gift/gift/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/sebastian/coderslab/PYTHON/Projekt_gift/gift/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ PYTHON/Projekt_gift/oddam/Oddam_w_dobre_rece/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OddamWDobreReceConfig(AppConfig):
+    name = 'Oddam_w_dobre_rece'
